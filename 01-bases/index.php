eaaadf53d6e310fbf6b16bfc087b8640342057c4
Matthieu Mota <matthieumota@gmail.com>
2019-02-05 14:40:38 +0100

    My first commit

---
+++ 01-bases/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP : Les bases</title>
+</head>
+<body>
+    <?php
+        // Ici, je peux écrire mon code PHP
+        echo '<h1>L\'titre</h1>';
+    ?>
+</body>
+</html>
---
+++ 02-variables/hello.php
@@ -0,0 +1,7 @@
+<?php
+
+$firstName = 'Matthieu';
+
+echo '<h1>Bonjour '.$firstName.'</h1>';
+// Interpolation de variable possible avec les doubles quotes
+echo "<h1>Bonjour $firstName</h1>";
