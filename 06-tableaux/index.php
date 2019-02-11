67e754a6ee4b65206961206312d17f9edea62858
Matthieu Mota <matthieumota@gmail.com>
2019-02-11 18:24:27 +0100

    Begin array php

---
+++ 06-tableaux/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$eleves = []; // Crée un tableau vide
+$eleves = array(); // Crée un tableau vide
+
+$eleves = [ // 1ère dimension
+    0 => [ // 2ème dimension
+        'nom' => 'Toto',
+        'notes' => [10, 8, 6, 2, 15, 2] // 3ème dimension
+    ],
+    1 => [ // 2ème dimension
+        'nom' => 'Titi',
+        'notes' => [4, 18, 20, 15, 13, 7]
+    ]
+];
+
+// Exemple pour parcourir un tableau
+foreach ($eleves as $eleve) {
+    echo $eleve['nom'];
+
+    // echo $eleve['notes'];
+
+    foreach ($eleve['notes'] as $note) {
+        echo $note . ',';
+    }
+}
+
+// Debuger un tableau
+print_r($eleves[0]['notes']);
