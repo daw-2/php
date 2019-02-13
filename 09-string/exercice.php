c1ed55ae084dcd51fa01aa016650d5ba8375ef13
Matthieu Mota <matthieumota@gmail.com>
2019-02-13 16:20:06 +0100

    prepare string exo php

---
+++ 09-string/exercice.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Acronyme : Créer une fonction qui prend en argument une chaine (World of Warcraft)
+et qui retourne les initiales de chaque mot en majuscule (WOW).
+*/
+echo acronym('World Of Warcraft').'<br />'; // WOW
+echo acronym('PHP: Hypertext Preprocessor').'<br />'; // PHP
+echo acronym('Hyper Text Markup Language').'<br />'; // HTML
+echo acronym('Cascading Style Sheet').'<br />'; // CSS
+
+
+/*
+Conjugaison : Créer une fonction qui permet de conjuguer un verbe
+(chercher par exemple). Cela doit renvoyer toutes les conjugaisons au présent.
+*/
+
+echo conjuguer('chercher');
+/*
+Je cherche
+Tu cherches
+Il cherche
+Nous cherchons
+Vous cherchez
+Ils cherchent
+*/
+echo conjugate('développer');
