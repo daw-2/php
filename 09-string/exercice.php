c0a99b0458c285c41a16264675ce1d5cad423e3b
Matthieu Mota <matthieumota@gmail.com>
2019-02-14 11:00:49 +0100

    Fix conjugate exo

--- 09-string/exercice.php
+++ 09-string/exercice.php
@@ -26,6 +26,42 @@ echo acronym('Cascading Style Sheet').'<br />'; // CSS
 Conjugaison : Créer une fonction qui permet de conjuguer un verbe
 (chercher par exemple). Cela doit renvoyer toutes les conjugaisons au présent.
 */
+
+function conjuguer($verbe) {
+    $racine = substr($verbe, 0, -2); // cherch
+    $terminaison = substr($verbe, -2); // er
+
+    $sujets = [
+        'Je' => 'e',
+        'Tu' => 'es',
+        'Il / Elle / On' => 'e',
+        'Nous' => 'ons',
+        'Vous' => 'ez',
+        'Ils / Elles' => 'ent'
+    ];
+
+    // j' exception (ajouter, inclure, enlever)
+    $voyelles = ['a', 'e', 'i', 'o', 'u', 'y'];
+    // Est-ce que la première lettre de la racine est une voyelle ?
+    if ( in_array(substr($racine, 0, 1), $voyelles) ) {
+        unset($sujets['Je']); // On supprime le Je du tableau
+        // $sujets['J\''] = 'e';
+        $sujets = array_merge(['J\'' => 'e'], $sujets); // On peut fusionner des tableaux
+    }
+
+    // *g exception (chang, mang, plong)
+    if (substr($racine, -1) === 'g') {
+        $sujets['Nous'] = 'eons';
+    }
+
+    $html = '';
+    foreach ($sujets as $pronom => $conjugaison) {
+        $html .= $pronom . ' ' . $racine . $conjugaison . '<br />';
+    }
+
+    return $html;
+}
+
 echo conjuguer('chercher');
 /*
 Je cherche
@@ -36,3 +72,5 @@ Vous cherchez
 Ils cherchent
 */
 echo conjuguer('développer');
+echo conjuguer('ajouter');
+echo conjuguer('manger');
