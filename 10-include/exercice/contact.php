f8e75ebdbb2651591821a258f9e5e4e1f17cdc95
Matthieu Mota <matthieumota@gmail.com>
2019-02-14 12:37:58 +0100

    Include fix

---
+++ 10-include/exercice/contact.php
@@ -0,0 +1,9 @@
+<?php include 'partials/header.php'; ?>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+      <h1 class="mt-5">Contact</h1>
+      <p>Ma page de contact</p>
+    </main>
+
+<?php include 'partials/footer.php'; ?>
