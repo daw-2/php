077dba2e6a9b287f8d6d019384377744ad4b7de0
Matthieu Mota <matthieumota@gmail.com>
2019-02-18 15:56:57 +0100

    Insert movie fix

--- 12-mysql/CRUD.sql
+++ 12-mysql/CRUD.sql
@@ -1,17 +1,17 @@
 -- Un commentaire en SQL
 -- CECI EST UN SUPPORT POUR LE COURS
 -- Insèrer des catégories de films
--- Action, Horreur, Film de gangsters, Science-fiction, Thriller
+-- Film de gangsters, Action, Horreur, Science-fiction, Thriller
 -- Copier/coller la requête SQL dans PHPMyAdmin (Onglet SQL)
 -- On peut mettre les colonnes entre `` (backtick => alt gr + 7)
 -- et les valeurs entre '' (quote)
 
-INSERT INTO category (`name`) VALUES ('Action');
+INSERT INTO category (`name`) VALUES ('Film de gangsters');
 
 -- On insère plusieurs catégories
 INSERT INTO category (`name`) VALUES
+('Action'),
 ('Horreur'),
-('Film de gangsters'),
 ('Science-fiction'),
 ('Thriller');
 
