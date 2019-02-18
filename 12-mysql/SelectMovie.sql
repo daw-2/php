d436479fe331a6fba7f70e932f0370329ba5e615
Matthieu Mota <matthieumota@gmail.com>
2019-02-18 16:20:24 +0100

    Exo select

---
+++ 12-mysql/SelectMovie.sql
@@ -0,0 +1,21 @@
+-- Récupère tous les films
+
+-- Récupère tous les films dans la catégorie "Films de gangster"
+
+-- Récupère tous les films dans la catégorie "Films de gangster" qui sont sortis avant 1990
+
+-- Récupère tous les films du plus récent au plus vieux
+
+-- Récupère les films dans l'ordre aléatoire
+
+-- Récupère les 10 premiers films à partir du 4ème
+
+-- Récupère le film le plus récent
+
+-- Récupère le film le plus ancien
+
+-- Récupère le film le plus récent et le plus ancien
+
+-- Compte le nombre de films
+
+-- Avoir la moyenne des années de sortie des films
