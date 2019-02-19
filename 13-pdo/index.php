54af1b8cfdfb471c7b9e97f5cc3b6b6f3a87d61c
Matthieu Mota <matthieumota@gmail.com>
2019-02-19 16:35:27 +0100

    PDO & const

--- 13-pdo/index.php
+++ 13-pdo/index.php
@@ -4,9 +4,16 @@
 // $db = new PDO('mysql:host=localhost;dbname=netflix2', 'root', '');
 // $db = new PDO('mysql:host=mysql.docker;port=3306;dbname=netflix2', 'root', 'root');
 
+// A remplacer par vos accès
+// Permet de définir des constantes (Des variables qui ne varient pas)
+define('DB_HOST', 'mysql.docker');
+define('DB_NAME', 'netflix2');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
+
 // On peut utiliser le bloc try catch pour attraper une erreur (exception) si elle se produit
 try {
-    $db = new PDO('mysql:host=mysql.docker;port=3306;dbname=netflix2', 'root', 'roo');
+    $db = new PDO('mysql:host='.DB_HOST.';port=3306;dbname='.DB_NAME, DB_USER, DB_PASSWORD);
 } catch (Exception $e) {
     echo '<span style="color: red">'.$e->getMessage().'</span>';
     echo '<p>Qu\'est-ce que tu as foutu à la ligne '.$e->getTrace()[0]['line'].' ?</p>';
