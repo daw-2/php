66f18fc0ced270bc0341c37e63a185aa5f56d370
Matthieu Mota <matthieumota@gmail.com>
2019-02-20 12:15:13 +0100

    Fix exo sql get

--- 13-pdo/movie.php
+++ 13-pdo/movie.php
@@ -20,3 +20,22 @@ $db = new PDO(
 // Exemple : Si je saisis movie.php?id=4, la requête doit
 // récupérer le film avec l'id 4
 // Sur la page, on affichera le titre du film récupéré
+
+if (!empty($_GET['id'])) { // Si l'id existe dans l'url
+    $id = intval($_GET['id']); // On récupére l'id dans l'URL
+} else {
+    die('Error'); // On arrête le script PHP et on affiche un message
+}
+// Aide pour la requête
+echo 'SELECT * FROM movie WHERE id = '. $id . '<br />';
+// On fait la requête
+$query = $db->query('SELECT * FROM movie WHERE id = '. $id);
+// On récupére UN film avec fetch
+$movie = $query->fetch();
+// Si le film existe en BDD
+if ($movie) {
+    // On affiche le nom du film
+    echo $movie['name'];
+} else {
+    echo 'Le film n\'existe pas';
+}
