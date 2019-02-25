79d194d434b0d6e753582e8326d1f7e995ecd430
Matthieu Mota <matthieumota@gmail.com>
2019-02-25 12:06:42 +0100

    Instructions for categories

---
+++ 14-webflix/public/category_list.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Sur cette page, on affichera la liste des catégories. Il n'y aura pas la sidebar ni le carousel.
+ * On pourra cliquer sur une catégorie, par exemple, si on clique sur la catégorie 5, on va sur
+ * le lien category_single?id=5.
+ */
---
+++ 14-webflix/public/category_single.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * On affiche la liste des films pour une seule catégorie. On n'affiche pas le carousel mais on affiche
+ * bien la sidebar sur le côté.
+ */
