0ef8d2344cbdaae37aa92ca8974f551839b9bce2
Matthieu Mota <matthieumota@gmail.com>
2019-02-25 16:50:27 +0100

    Fix movie add

--- 14-webflix/public/movie_add.php
+++ 14-webflix/public/movie_add.php
@@ -13,3 +13,50 @@
  * 
  * Quand le formulaire sera soumis, on récupère tous les champs du formulaire en PHP. On les vérifie et s'ils sont corrects, on exécute la requête SQL pour insérer le film en BDD. Optionnellement, on pourra également faire l'upload de la jaquette.
  */
+
+require_once __DIR__ . '/../partials/header.php'; ?>
+
+<div class="container my-5">
+    <div class="row">
+        <div class="col-md-6 offset-3">
+            <form action="" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="name">Titre</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="date">Date de sortie</label>
+                    <input type="date" name="date" id="date" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="description">Description</label>
+                    <textarea name="description" id="description" class="form-control"></textarea>
+                </div>
+
+                <div class="form-group">
+                    <label for="cover">Jaquette</label>
+                    <input type="file" name="cover" id="cover" class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="category_id">Catégorie</label>
+                    <select name="category_id" id="category_id" class="form-control">
+                        <?php
+                            $query = $db->query('SELECT * FROM category');
+                            $categories = $query->fetchAll();
+                            foreach ($categories as $category) {
+                                echo '<option value="'.$category['id'].'">'.$category['name'].'</option>';
+                            }
+                        ?>
+                    </select>
+                </div>
+
+                <button class="btn btn-primary btn-block">Ajouter le film</button>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php require_once __DIR__ . '/../partials/footer.php';
