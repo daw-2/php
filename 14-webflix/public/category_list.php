7604a9756380539671f77177e4ca790966934855
Matthieu Mota <matthieumota@gmail.com>
2019-02-25 12:15:08 +0100

    Fix typo

--- 14-webflix/public/category_list.php
+++ 14-webflix/public/category_list.php
@@ -3,5 +3,5 @@
 /**
  * Sur cette page, on affichera la liste des catégories. Il n'y aura pas la sidebar ni le carousel.
  * On pourra cliquer sur une catégorie, par exemple, si on clique sur la catégorie 5, on va sur
- * le lien category_single?id=5.
+ * le lien category_single.php?id=5.
  */
