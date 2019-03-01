3c9fc6358441df87110ba6a9fd115e5c483f4bea
Matthieu Mota <matthieumota@gmail.com>
2019-03-01 16:57:57 +0100

    Finish ajax exo

--- 16-ajax-jquery/form.php
+++ 16-ajax-jquery/form.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * 1. Ecouter l'événement submit du formulaire en JS
- * 2. Dans cet événement, on exécutera une requête en AJAX en POST sur form.php avec les données du formulaire.
- * 3. On récupérera firstname et lastname dans $_POST
- * 4. On cherchera ensuite dans le tableau le contact qui a exactement le même nom et le même prénom que ce qui a été saisi.
- * 5. Afficher le code secret et le récupérer sur la page HTML uniquement si le nom et le prénom correspondent.
- */
-
 $contacts = [
     [
         'lastname' => 'Mota',
@@ -25,3 +17,24 @@ $contacts = [
         'secret' => 'a09fabc53cb'
     ]
 ];
+
+/**
+ * 1. Ecouter l'événement submit du formulaire en JS
+ * 2. Dans cet événement, on exécutera une requête en AJAX en POST sur form.php avec les données du formulaire.
+ * 3. On récupérera firstname et lastname dans $_POST
+ * 4. On cherchera ensuite dans le tableau le contact qui a exactement le même nom et le même prénom que ce qui a été saisi.
+ * 5. Afficher le code secret et le récupérer sur la page HTML uniquement si le nom et le prénom correspondent.
+ */
+
+if ('POST' === $_SERVER['REQUEST_METHOD']) {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+
+    // echo $firstname . $lastname;
+
+    foreach ($contacts as $contact) {
+        if ($contact['firstname'] === $firstname && $contact['lastname'] === $lastname) {
+            echo $contact['secret'];
+        }
+    }
+}
